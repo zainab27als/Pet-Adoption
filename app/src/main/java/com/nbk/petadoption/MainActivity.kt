2c43067c8604851f9aedb6692ad607cc6551ce2b
Zainab <zainabalsaffar03@gmail.com>
2025-05-26 01:19:26 +0300

    no changes made, only cleaned the code

--- app/src/main/java/com/nbk/petadoption/MainActivity.kt
+++ app/src/main/java/com/nbk/petadoption/MainActivity.kt
@@ -3,21 +3,9 @@ package com.nbk.petadoption
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nbk.petadoption.ui.theme.PetAdoptionTheme
 import com.nbk.petadoption.viewmodel.PetViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.nbk.petadoption.composables.PetCard
 import com.nbk.petadoption.composables.PetListScreen
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/nbk/petadoption/composables/PetCard.kt
+++ app/src/main/java/com/nbk/petadoption/composables/PetCard.kt
@@ -1,6 +1,5 @@
 package com.nbk.petadoption.composables
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
